fe854393ad2836c682f445e5cabed8bf103a2a1e
Felipe Reis <felipereisdesantana@yahoo.com.br>
2022-11-11 16:00:22 -0300

    requisito 8 em andamento ainda

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ShoppingCart extends Component {
   state = {
     cartItems: [],
-    // cartItemsReduced: [],
+    cartItemsReduced: [],
   };
 
   componentDidMount() {
@@ -11,7 +11,7 @@ class ShoppingCart extends Component {
     this.reduceCartItems();
   }
 
-  reduceCartItems = () => {
+  sortCartItems = () => {
     const { cartItems } = this.state;
     const MINUS_ONE = -1;
     const itemsSorted = cartItems
@@ -24,27 +24,49 @@ class ShoppingCart extends Component {
         }
         return 0;
       });
-    console.log(itemsSorted);
+    this.setState({ cartItemsReduced: itemsSorted }, this.reduceCartItems);
+  };
+
+  reduceCartItems = () => {
+    const { cartItemsReduced } = this.state;
+    const result = cartItemsReduced.reduce((previousValue, currentValue) => {
+      if (previousValue.some(({ id }) => id === currentValue.id)) {
+        return [...previousValue];
+      }
+      return [...previousValue, currentValue];
+    }, []);
+    const mapFirst = result.map(({ id }) => {
+      const map = cartItemsReduced.reduce((previousValue, currentValue) => {
+        if (currentValue.id === id) return previousValue + 1;
+        return previousValue;
+      }, 0);
+      return map;
+    });
+    const mapSecond = result.map((obj, index) => {
+      obj.quantity = mapFirst[index];
+      return obj;
+    });
+    this.setState({ cartItemsReduced: mapSecond });
   };
 
   restoreCartProducts = () => {
     const getCartProducts = JSON.parse(localStorage.getItem('cart'));
     if (getCartProducts) {
-      this.setState({ cartItems: getCartProducts }, this.reduceCartItems);
+      this.setState({ cartItems: getCartProducts }, this.sortCartItems);
     }
   };
 
   render() {
-    const { cartItems } = this.state;
+    const { cartItemsReduced } = this.state;
     return (
       <div>
-        {cartItems.length === 0 ? (
+        {cartItemsReduced.length === 0 ? (
           <h4
             data-testid="shopping-cart-empty-message"
           >
             Seu carrinho está vazio
           </h4>) : (
-          cartItems.map((product, index) => (
+          cartItemsReduced.map((product, index) => (
             <div key={ `${product.id}${index}` }>
               <img src={ product.thumbnail } alt={ product.title } />
               <h6 data-testid="shopping-cart-product-name">{ product.title }</h6>
