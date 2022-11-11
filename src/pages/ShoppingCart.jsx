78bc55d75a8732191622453a307d0064725db7f6
Felipe Reis <felipereisdesantana@yahoo.com.br>
2022-11-10 21:47:37 -0300

    requisito 8 em andamento

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -3,16 +3,34 @@ import React, { Component } from 'react';
 class ShoppingCart extends Component {
   state = {
     cartItems: [],
+    // cartItemsReduced: [],
   };
 
   componentDidMount() {
     this.restoreCartProducts();
+    this.reduceCartItems();
   }
+
+  reduceCartItems = () => {
+    const { cartItems } = this.state;
+    const MINUS_ONE = -1;
+    const itemsSorted = cartItems
+      .sort((a, b) => {
+        if ((a.id > b.id)) {
+          return 1;
+        }
+        if (b.id > a.id) {
+          return MINUS_ONE;
+        }
+        return 0;
+      });
+    console.log(itemsSorted);
+  };
 
   restoreCartProducts = () => {
     const getCartProducts = JSON.parse(localStorage.getItem('cart'));
     if (getCartProducts) {
-      this.setState({ cartItems: getCartProducts });
+      this.setState({ cartItems: getCartProducts }, this.reduceCartItems);
     }
   };
 
@@ -26,8 +44,8 @@ class ShoppingCart extends Component {
           >
             Seu carrinho está vazio
           </h4>) : (
-          cartItems.map((product) => (
-            <div key={ product.id }>
+          cartItems.map((product, index) => (
+            <div key={ `${product.id}${index}` }>
               <img src={ product.thumbnail } alt={ product.title } />
               <h6 data-testid="shopping-cart-product-name">{ product.title }</h6>
               <h6>{ product.price }</h6>
